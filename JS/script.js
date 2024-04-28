ba08dd6216886b360fbfadeb94adaf1f239b0de4
Felilpz <luizfelipe.vieira12@gmail.com>
2024-04-28 20:03:34 -0300

    need to fix bug trying to delete 2 or more numbers in screen

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,24 @@
-// calculadora javascript
-
 let total = 0;
 let buffer = "0";
 let previousOperator;
-let screen = document.querySelector('.screen')
+let screen = document.querySelector('.screen');
+console.log(screen);
 
 function buttonClick(value) {
     if (isNaN(value)) {
         handleSymbol(value);
     } else {
-        handleSymbol(value);
+        handleNumber(value);
     }
     screen.innerText = buffer;
+}
+
+function handleNumber(numberString) {
+    if (buffer === "0") {
+        buffer = numberString;
+    } else {
+        buffer += numberString;
+    }
 }
 
 function handleSymbol(symbol) {
@@ -34,13 +41,52 @@ function handleSymbol(symbol) {
                 buffer = '0';
             } else {
                 buffer = buffer.toString(0, buffer.length - 1);
-            };
+            }
+            console.log('ativou')
             break;
         case '+':
-        case '-':
+        case '−':
         case '×':
         case '÷':
             handleMath(symbol);
             break;
     }
 }
+
+function handleMath(symbol) {
+    if (buffer === '0') {
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if (total === 0) {
+        total = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = symbol;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+    if (previousOperator === '+') {
+        total += intBuffer;
+    } else if (previousOperator === '−') {
+        total -= intBuffer;
+    } else if (previousOperator === '×') {
+        total *= intBuffer;
+    } else if (previousOperator === '÷') {
+        total /= intBuffer;
+    }
+
+}
+
+function init() {
+    document.querySelector('.calc-buttons').addEventListener('click', function (event) {
+        buttonClick(event.target.innerText);
+    });
+}
+
+init();
